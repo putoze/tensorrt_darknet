e378ac9d9535d1f93e6fb86f13140bb93a874d54
potoze <kim4501@gmail.com>
2023-08-09 18:45:08 +0800

    success work on xavier

---
+++ excute_cam_trt.sh
@@ -0,0 +1,1 @@
+python3 trt_yolo.py -m yolov3-tiny-mid_eyetracker --gstr 1 -c 5 -t 0.98 #--width 1280 --height 722
---
+++ excute_cam_trt_usb.sh
@@ -0,0 +1,1 @@
+python3 trt_yolo.py -m yolov3-tiny-mid_eyetracker --usb 8 -c 5 #--width 1280 --height 722
